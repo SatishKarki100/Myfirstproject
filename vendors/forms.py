237c56327ed5c4eaed361e387dc9f277c0c4df39
SatishKarki100 <satishkarki9090@gmail.com>
2026-09-21 08:28:58 +0545

    updated

--- vendors/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import VendorStore
-
-class VendorStoreForm(forms.ModelForm):
-    class Meta:
-        model = VendorStore
-        fields = ['store_name', 'description', 'business_email']
